ee215475c3dc1dbb2d956a665c08368bd59d8a21
Gabriel Maldonado Colón <44788342+Gamaco@users.noreply.github.com>
2024-02-11 23:14:02 -0400

    functional.

---
+++ static/js/inventory.js
@@ -0,0 +1,9 @@
+document.getElementById('searchInput').addEventListener('input', function () {
+  const searchText = this.value.toLowerCase();
+  const tableRows = document.querySelectorAll('#InventoryTable tbody tr');
+
+  tableRows.forEach(row => {
+    const rowData = row.textContent.toLowerCase();
+    row.style.display = rowData.includes(searchText) ? '' : 'none';
+  });
+});
