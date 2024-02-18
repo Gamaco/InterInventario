d06a15b7a5b7a270edb16f647ec177e19356d687
Gabriel Maldonado Colón <44788342+Gamaco@users.noreply.github.com>
2024-02-18 16:01:10 -0400

    Added a table.

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -7,3 +7,16 @@ document.getElementById('searchInput').addEventListener('input', function () {
     row.style.display = rowData.includes(searchText) ? '' : 'none';
   });
 });
+
+  // Add event listener to the dropdown items
+  categoryDropdown.addEventListener('click', function(event) {
+  // Get the button and dropdown elements
+  var dropdownButton = document.querySelector('.dropdown-toggle');
+  var categoryDropdown = document.getElementById('categoryDropdown');
+
+    // Check if a dropdown item was clicked
+    if (event.target.classList.contains('dropdown-item')) {
+      // Update the button text with the selected item
+      dropdownButton.innerText = event.target.innerText;
+    }
+  });
