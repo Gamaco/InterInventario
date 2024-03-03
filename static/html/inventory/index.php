92b985ba17eb3d8a0e1f446c4d3e7e5d81953d5b
Gabriel Maldonado Colón <44788342+Gamaco@users.noreply.github.com>
2024-03-02 21:23:01 -0400

    Inventory index page buttons positioning fixed.

--- static/html/inventory/index.php
+++ static/html/inventory/index.php
@@ -43,131 +43,124 @@
         <?php include '../components/navbar.php'; ?>
 
         <main class="content">
-            <div class="container-fluid p-0">
+            <div class="container-fluid">
                 <h1 class="h3 mb-3"><strong>Inventory</strong> List</h1>
                 <div class="row">
-                    <div class="col-12 col-lg-14 col-xxl-12 d-flex">
-                        <div class="card flex-fill">
-                            <div class="card-body mt-0">
-                                <h5 class="card-title mb-2">Options</h5>
-                                <div class="row mt-0">
-                                    <div class="col-md-auto mb-2 mb-md-0 d-flex flex-column flex-md-row align-items-start align-items-md-center">
-                                        <a class="btn btn-primary btn-lg fs-5 me-md-2 mb-2 mb-md-0" href="../inventory/create.php"><i class="fa fa-plus" aria-hidden="true"></i> Add Product</a>
-                                        <a class="btn btn-lg fs-5 text-dark btn-secondary me-md-2" href="../inventory/create-category.php"><i class="fa fa-code" aria-hidden="true"></i> Manage Categories</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-md-auto mb-2 mb-md-0 d-flex align-items-center">
+                        <a class="btn btn-primary btn-lg fs-5 me-md-2 me-2" href="../inventory/create.php"><i class="fa fa-plus" aria-hidden="true"></i> Add Product</a>
+                        <a class="btn btn-lg fs-5 text-dark btn-secondary" href="../inventory/create-category.php"><i class="fa fa-code" aria-hidden="true"></i> Manage Categories</a>
                     </div>
                 </div>
+            </div>
+        </main>
 
-                <div class="row">
-                    <div class="col-12 col-lg-14 col-xxl-12 d-flex">
-                        <div class="card flex-fill">
-                            <div class="card-body">
-                                <div class="row align-items-center">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <div class="input-group">
-                                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
-                                            <input type="text" id="searchInput" class="form-control fs-4" placeholder="Search">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6 d-flex justify-content-center justify-content-sm-end">
-                                        <div class="dropdown-center">
-                                            <button class="btn btn-secondary text-dark btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                Categories
-                                            </button>
-                                            <ul class="dropdown-menu" id="categoryDropdown">
-                                                <?php
-                                                include '../../db/config.php';
-
-                                                $query = "CALL GetCategories()";
-
-                                                $stmt = $connection->prepare($query);
-
-                                                if (!$stmt) {
-                                                    die("Error preparing statement: " . $connection->error);
-                                                }
-
-                                                $result = $stmt->execute();
-
-                                                if (!$result) {
-                                                    die("Error executing statement: " . $stmt->error);
-                                                }
-
-                                                $stmt->bind_result($id, $category);
-
-                                                while ($stmt->fetch()) {
-                                                    echo "<li><a class='dropdown-item'>" . $category . "</a></li>";
-                                                }
-
-                                                $stmt->close();
-                                                $connection->close();
-                                                ?>
-
-                                            </ul>
-                                        </div>
-                                    </div>
+        <div class="row">
+            <div class="col-12 col-lg-14 col-xxl-12 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
+                                    <input type="text" id="searchInput" class="form-control fs-4" placeholder="Search">
                                 </div>
                             </div>
-
-                        </div>
-
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-lg-14 col-xxl-12 d-flex">
-                        <div class="card flex-fill">
-                            <div class="card-header">
-                                <div class="row mt-3">
-                                    <span class="badge badge-secondary responsive-badge fs-5 p-4" id="displayedRowCount"></span>
-                                </div>
-
-                            </div>
-
-
-                            <div class="table-container">
-                                <table id="InventoryTable" class="table my-0 table-hover border-secondary">
-                                    <thead>
-                                        <tr>
-                                            <th>Description</th>
-                                            <th>PTag</th>
-                                            <th>GN</th>
-                                            <th>Model</th>
-                                            <th>Serial No</th>
-                                            <th>Fund</th>
-                                            <th>AC</th>
-                                            <th>CL</th>
-                                            <th>F</th>
-                                            <th>AQU</th>
-                                            <th>ST</th>
-                                            <th>Acquisition</th>
-                                            <th>Received</th>
-                                            <th>Doc No</th>
-                                            <th>Amt</th>
-                                            <th>Location</th>
-                                            <th>Options</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                            <div class="col-sm-6 d-flex justify-content-center justify-content-sm-end">
+                                <div class="dropdown-center">
+                                    <button class="btn btn-secondary text-dark btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Categories
+                                    </button>
+                                    <ul class="dropdown-menu" id="categoryDropdown">
                                         <?php
                                         include '../../db/config.php';
 
-                                        $query = "CALL GetAllEquipos()"; // Calling the stored procedure to fetch inventory data
+                                        $query = "CALL GetCategories()";
 
-                                        $equipos = $connection->query($query);
+                                        $stmt = $connection->prepare($query);
 
-                                        // In case the query failed
-                                        if (!$equipos) {
-                                            die("Invalid query: " . $connection->error);
+                                        if (!$stmt) {
+                                            die("Error preparing statement: " . $connection->error);
                                         }
 
-                                        // Reads data and also checks if the field value is empty.
-                                        // In case the field is empty, it'll display N/A on the table,
-                                        // to avoid having empty spaces on the table.
-                                        // Empty spaces make the table collapse/wrap a field into another.
-                                        while ($equipo = $equipos->fetch_assoc()) {
-                                            echo "
+                                        $result = $stmt->execute();
+
+                                        if (!$result) {
+                                            die("Error executing statement: " . $stmt->error);
+                                        }
+
+                                        $stmt->bind_result($id, $category);
+
+                                        while ($stmt->fetch()) {
+                                            echo "<li><a class='dropdown-item'>" . $category . "</a></li>";
+                                        }
+
+                                        $stmt->close();
+                                        $connection->close();
+                                        ?>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-14 col-xxl-12 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-header">
+                        <div class="row mt-3">
+                            <span class="badge badge-secondary responsive-badge fs-5 p-4" id="displayedRowCount"></span>
+                        </div>
+
+                    </div>
+
+
+                    <div class="table-container">
+                        <table id="InventoryTable" class="table my-0 table-hover border-secondary">
+                            <thead>
+                                <tr>
+                                    <th>Description</th>
+                                    <th>PTag</th>
+                                    <th>GN</th>
+                                    <th>Model</th>
+                                    <th>Serial No</th>
+                                    <th>Fund</th>
+                                    <th>AC</th>
+                                    <th>CL</th>
+                                    <th>F</th>
+                                    <th>AQU</th>
+                                    <th>ST</th>
+                                    <th>Acquisition</th>
+                                    <th>Received</th>
+                                    <th>Doc No</th>
+                                    <th>Amt</th>
+                                    <th>Location</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                include '../../db/config.php';
+
+                                $query = "CALL GetAllEquipos()"; // Calling the stored procedure to fetch inventory data
+
+                                $equipos = $connection->query($query);
+
+                                // In case the query failed
+                                if (!$equipos) {
+                                    die("Invalid query: " . $connection->error);
+                                }
+
+                                // Reads data and also checks if the field value is empty.
+                                // In case the field is empty, it'll display N/A on the table,
+                                // to avoid having empty spaces on the table.
+                                // Empty spaces make the table collapse/wrap a field into another.
+                                while ($equipo = $equipos->fetch_assoc()) {
+                                    echo "
                                             <tr>
                                                 <td data-label='Description'>" . ($equipo['Description'] ? $equipo['Description'] : 'N/A') . "</td>
                                                 <td data-label='PTag'>" . ($equipo['Ptag'] ? $equipo['Ptag'] : 'N/A') . "</td>
@@ -190,39 +183,39 @@
                                                     <a class='btn btn-danger rounded-3 btn-lg' style='width: 100px;' data-bs-toggle='modal' data-bs-target='#itemDeletionModal' data-item-id='$equipo[id]'><i class='fa fa-trash-o' aria-hidden='true'></i> Delete</a>
                                                 </td>
                                             </tr>";
-                                        }
+                                }
 
-                                        $connection->close();
-                                        ?>
+                                $connection->close();
+                                ?>
 
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <!-- item Deletion Modal -->
-        <div class="modal fade" id="itemDeletionModal" tabindex="-1" aria-labelledby="itemDeletionModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="itemDeletionModalLabel">Confirm Deletion</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>You are about to delete this item. This action cannot be undone.</p>
-                        <p>Are you sure you want to proceed?</p>
-                        <input type="hidden" id="itemIdToDelete" name="deleteItemId">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary text-dark" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-danger" id="confirmDeleteBtn">Confirm</button>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    </main>
+    <!-- item Deletion Modal -->
+    <div class="modal fade" id="itemDeletionModal" tabindex="-1" aria-labelledby="itemDeletionModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="itemDeletionModalLabel">Confirm Deletion</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>You are about to delete this item. This action cannot be undone.</p>
+                    <p>Are you sure you want to proceed?</p>
+                    <input type="hidden" id="itemIdToDelete" name="deleteItemId">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary text-dark" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger" id="confirmDeleteBtn">Confirm</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     </div>
     </div>
